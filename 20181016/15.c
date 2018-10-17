0f0c1dddb6e42fb5e9a329b3a7a09e9801d7dc05
Luzi Qi <1098004807@qq.com>
2018-10-17 07:42:34 -0700

    homework-19180421

--- 20181016/15.c
+++ 20181016/15.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main ()
 {
 int x;
 scanf("%d",&x);
-printf("%d,%o,%x\n",x,x,x);
+char string[25];
+itoa(x, string,2);
+printf("%s,%o,%x\n",string,x,x);
 return 0;
 } 
