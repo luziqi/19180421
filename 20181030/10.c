c22cc8f6084011013cede7708c01c5230197727d
Luzi Qi <1098004807@qq.com>
2018-11-04 19:16:11 -0800

    homework-4

--- 20181030/10.c
+++ 20181030/10.c
@@ -14,19 +14,15 @@ int is_str_pal(const char* str)
  {
  	int a,b=0;
  	a = strlen(str);
- 	if(a%2==0)
- 	return -1;
- 	else
+ 	
+ 	for(int i=0;i<=(a-1)/2;i++)
  	{
- 		for(int i=0;i<=(a-1)/2;i++)
- 	{
- 		if(str[i]!=str[a-i-1])
- 		b++;
-	 }
+           if(str[i]!=str[a-i-1])
+ 	   b++;
+        }
 	 if(b==0)
 	 return 0;
 	 else
-	 return -1;
-	 }
+	 return -1;	 
 	 
  }
